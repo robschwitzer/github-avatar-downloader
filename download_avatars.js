1291df3faf6d2fd8e393e29cc14322d0d6920388
Rob Schwitzer <deadinthewater@me.com>
2018-05-15 06:22:51 +0000

    downloads avatars through hardcoded params

--- download_avatars.js
+++ download_avatars.js
@@ -32,8 +32,12 @@ function downloadImageByURL(url, filePath) {
 }
 
 getRepoContributors('jquery', 'jquery', function(err, result) {
-  console.log('Errors:', err);
-  console.log('Result', result);
+  for (var i = 0; i < result.length; i++) {
+    var contributor = result[i];
+    downloadImageByURL(contributor.avatar_url, 'avatars/' + contributor.login + '.jpg');
+  }
+  // console.log('Errors:', err);
+  // console.log('Result', result);
 });
 
 function cb () {
@@ -41,4 +45,3 @@ function cb () {
 
 }
 
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
